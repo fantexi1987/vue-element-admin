4a6057ad9b8f73f24d1238238f6f19599e9c64e1
fantexi1987 <184606428@qq.com>
2018-12-12 10:45:06 +0800

    delete spaces

--- src/main.js
+++ src/main.js
@@ -6,17 +6,14 @@ import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 import '@/styles/index.scss' // global css
-
 import App from './App'
 import router from './router'
 import store from './store'
-
 import i18n from './lang' // Internationalization
 import './icons' // icon
 import './errorLog'// error log
 import './permission' // permission control
 import './mock' // simulation data
-
 import * as filters from './filters' // global filters
 
 Vue.use(Element, {
